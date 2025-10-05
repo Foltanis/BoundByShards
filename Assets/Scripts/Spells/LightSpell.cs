fd5a12295171e4a5ce73de74daa41d662ba01504
Jan Lenhart <jan.lenhart2003@gmail.com>
2025-10-05 17:37:49 +0200

    merge fix fix

--- Assets/Scripts/Spells/LightSpell.cs
+++ Assets/Scripts/Spells/LightSpell.cs
@@ -13,52 +13,52 @@ public class LightSpell : Spells
 
     public override void Awake()
     {
-        base.Awake();
-        // naËÌtame vstupy z maga
-        Light2Move = mage.GetSpellMoveInput();
-        Light1Move = mage.GetMoveAction();
+        //base.Awake();
+        //// naËÌtame vstupy z maga
+        //Light2Move = mage.GetSpellMoveInput();
+        //Light1Move = mage.GetMoveAction();
     }
 
     private void Update()
     {
-        // sledujeme, Ëi bolo stlaËenÈ X
-        if (lightToggle.triggered)
-        {
-            if (active)
-                EndSpell();
-            else
-                Cast();
-        }
-
-        // prÌpadne sem mÙûeme neskÙr pridaù pohyb svetiel
+        //// sledujeme, Ëi bolo stlaËenÈ X
+        //if (lightToggle.triggered)
+        //{
+        //    if (active)
+        //        EndSpell();
+        //    else
+        //        Cast();
+        //}
+        //
+        //// prÌpadne sem mÙûeme neskÙr pridaù pohyb svetiel
     }
 
     public override void Cast()
     {
-        if (active) return; // ak uû beûÌ, nerob niË
-        active = true;
-
-        // zablokujeme pohyb m·ga
-        mage.SetControlEnabled(false);
-
-        // pozÌcia maga
-        Vector3 pos = mage.transform.position;
-
-        // vytvorÌme 2 svetl·
-        light1 = Instantiate(lightPrefab, pos + new Vector3(-1, 0, 0), Quaternion.identity);
-        light2 = Instantiate(lightPrefab, pos + new Vector3(1, 0, 0), Quaternion.identity);
-
-        // napr. prid·me skripty na ovl·danie svetiel
-        light1.AddComponent<LightController>().Init(LightController.ControlType.WASD);
-        light2.AddComponent<LightController>().Init(LightController.ControlType.Spell);
+        //if (active) return; // ak uû beûÌ, nerob niË
+        //active = true;
+        //
+        //// zablokujeme pohyb m·ga
+        //mage.SetControlEnabled(false);
+        //
+        //// pozÌcia maga
+        //Vector3 pos = mage.transform.position;
+        //
+        //// vytvorÌme 2 svetl·
+        //light1 = Instantiate(lightPrefab, pos + new Vector3(-1, 0, 0), Quaternion.identity);
+        //light2 = Instantiate(lightPrefab, pos + new Vector3(1, 0, 0), Quaternion.identity);
+        //
+        //// napr. prid·me skripty na ovl·danie svetiel
+        //light1.AddComponent<LightController>().Init(LightController.ControlType.WASD);
+        //light2.AddComponent<LightController>().Init(LightController.ControlType.Spell);
     }
 
     private void EndSpell()
     {
-        active = false;
-        mage.SetControlEnabled(true);
+        //active = false;
+        //mage.SetControlEnabled(true);
 
-        if (light1) Destroy(light1);
-        if (light2) Destroy(light2);
+        //if (light1) Destroy(light1);
+        //if (light2) Destroy(light2);
     }
 }
