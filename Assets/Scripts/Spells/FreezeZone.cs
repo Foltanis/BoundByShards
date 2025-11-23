0010916341136637a8d43aee1874d51f23f62468
Štefan Foltán <114149757+Foltanis@users.noreply.github.com>
2025-11-23 21:57:58 +0100

    freeze particles

--- Assets/Scripts/Spells/FreezeZone.cs
+++ Assets/Scripts/Spells/FreezeZone.cs
@@ -3,7 +3,18 @@ using UnityEngine;
 public class FreezeZone : MonoBehaviour
 {
     [SerializeField] private float freezeDuration = 3f;
+    [SerializeField] private ParticleSystem freezeParticles;
 
+    void Start()
+    {
+        if (freezeParticles != null)
+        {
+            var ps = Instantiate(freezeParticles, transform.position, Quaternion.identity);
+            ps.Play();
+
+            Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("FreezeZone hit: " + other.name);
