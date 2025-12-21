91894276657312629bd4431416f1a384fd4411ed
Jan Lenhart <jan.lenhart2003@gmail.com>
2025-12-21 23:35:28 +0100

    last changes

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -4,18 +4,9 @@ using UnityEngine.SceneManagement;
 public class Credits : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
-    [SerializeField] private SceneFadeOut sceneFadeOut;
     
     void Start()
     {
         audioSource.time = 175.8f;
-        StartCoroutine(WaitForAudioToEnd());
-    }
-
-    private System.Collections.IEnumerator WaitForAudioToEnd()
-    {
-        while (audioSource.isPlaying)
-            yield return null;
-        sceneFadeOut.FadeAndChangeScene();
     }
 }
